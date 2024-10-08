2c33c9a3ed4c3d51ab0d22559fe492eaf6a93d64
Karan Verma <hydroweaver@gmail.com>
2024-10-08 16:34:13 +0200

    added comment

--- initialInjection.js
+++ initialInjection.js
@@ -14,8 +14,23 @@ for (let i = 0; i < commentTree.length; i++) {
       .getElementsByClassName("comhead")[0]
       .insertAdjacentElement("afterend", empty_div.querySelector("button"));
   }
-} 
+}
 
+//GET BUTTONS AND ADD LISTENER AND REMOVE LATER DUDE!
+for (let i = 0; i < commentTree.length; i++) {
+  if (commentTree[i].getElementsByTagName("td")[1].getAttribute("indent") == "0") {
+    empty_div = document.createElement("div");
+    empty_div.innerHTML = JSON.parse(insertion);
+    commentTree[i]
+      .getElementsByClassName("comhead")[0]
+      .insertAdjacentElement("afterend", empty_div.querySelector("button"));
+  }
+}
+
+
+
+
+//Button needs to call this code on click handler - How?
 function summarizeButtonCommentAccumulator(buttonHead){
   let head = buttonHead.closest('.athing.comtr') //     head = $0.closest('.athing.comtr')
   let accumulated_comment = []
@@ -24,4 +39,5 @@ function summarizeButtonCommentAccumulator(buttonHead){
     console.log(Number(head.getElementsByTagName("td")[1].getAttribute("indent")),head);
     head = head.nextElementSibling
   }
+  accumulated_comment.push(head.querySelector(".comment").getElementsByTagName("div")[0].innerHTML)
 }
